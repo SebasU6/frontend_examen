6b08c1d326b23fc8377c13053961ed2e41193e5c
Sebastian <preteitor@gmail.com>
2024-05-28 10:58:50 -0400

    cambio 1

--- pages/index/components/appbar.vue
+++ pages/index/components/appbar.vue
@@ -2,7 +2,7 @@
     <v-app-bar :elevation="0" >
       <v-app-bar-title text @click="goTo('/')">Index</v-app-bar-title>
   
-        <v-btn text @click="goTo('/products')">Productos</v-btn>
+        <v-btn text @click="goTo('/products')">hola</v-btn>
   
         <v-btn @click="goTo('nosotros')">¿Quiénes somos?</v-btn>
   
