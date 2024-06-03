6e0b7ba6ed25970736f22fa6a8885c324caa74e7
Buc0ss <brunocoss2608@gmail.com>
2024-06-03 19:41:32 -0400

    cambios

--- pages/index/components/footer.vue
+++ pages/index/components/footer.vue
@@ -1,48 +1,37 @@
 <template>
-  <v-footer
-    class="wood-background text-center d-flex flex-column"
-  >
-    <div>
-      <v-btn
-        v-for="icon in icons"
-        :key="icon"
-        :icon="icon"
-        class="mx-4"
-        variant="text"
-      ></v-btn>
-    </div>
-
-    <div class="pt-0">
-      Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-    </div>
-
-    <v-divider></v-divider>
-
-    <div>
-      {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-    </div>
-  </v-footer>
-</template>
-
-<script>
-export default {
-  data: () => ({
-    icons: [
-      'mdi-facebook',
-      'mdi-twitter',
-      'mdi-linkedin',
-      'mdi-instagram',
-    ],
-  }),
-};
-</script>
-
-<style scoped>
-.wood-background {
-  background-color: #be7a43; /* Color madera abedul */
-  margin-top: -0.9cm; /* Eleva el borde superior del footer */
-  padding-top: 1cm; /* Ajusta el padding para mantener el contenido dentro del footer */
-  margin-bottom: -0.9cm; /* Eleva el borde superior del footer */
-  padding-bottom: 1.5cm;
-}
-</style>
+    <v-footer
+      class="bg-indigo-lighten-1 text-center d-flex flex-column"
+    >
+      <div>
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          :icon="icon"
+          class="mx-4"
+          variant="text"
+        ></v-btn>
+      </div>
+  
+      <div class="pt-0">
+        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      </div>
+  
+      <v-divider></v-divider>
+  
+      <div>
+        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+      </div>
+    </v-footer>
+  </template>
+  <script>
+    export default {
+      data: () => ({
+        icons: [
+          'mdi-facebook',
+          'mdi-twitter',
+          'mdi-linkedin',
+          'mdi-instagram',
+        ],
+      }),
+    }
+  </script>
