9eea059e97a82a035627ac90dcb1437920dfd4a2
YaBoiKenobi12 <angelmiguel1297@gmail.com>
2024-06-07 16:20:46 -0400

    nosotros fotos

--- pages/nosotros/components/about.vue
+++ pages/nosotros/components/about.vue
@@ -5,9 +5,21 @@
         <img src="https://via.placeholder.com/200x400" alt="Sample Image" />
       </template>
       <div>
-        <div class="text-h6">Content title 1</div>
+        <div class="text-h6">Sebastian Uzqueda</div>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+    </v-timeline-item>
+
+    <v-timeline-item>
+      <template v-slot:opposite>
+        <img src="file:///C:/Users/MIGUEL%20ANGEL/Desktop/foto%20bruno%20para%20programacion.jpg" alt="Sample Image" />
+      </template>
+      <div>
+        <div class="text-h6">Bruno Coss</div>
+        <p>
+          Nombre: Bruno Coss     Descripción: Estudiante de la carrera de Negocios y Ciencia de Datos                                            Nacimiento: 26 de agosto de 2005 Lugar: La Paz, Bolivia.              Colegio de egreso: San Ignacio 
         </p>
       </div>
     </v-timeline-item>
@@ -17,7 +29,7 @@
         <img src="https://via.placeholder.com/200x400" alt="Sample Image" />
       </template>
       <div>
-        <div class="text-h6">Content title 2</div>
+        <div class="text-h6">Gabriel Villarroel</div>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </p>
@@ -26,25 +38,12 @@
 
     <v-timeline-item>
       <template v-slot:opposite>
-        <img src="https://via.placeholder.com/200x400" alt="Sample Image" />
+        <img src="c:\Users\MIGUEL ANGEL\AppData\Local\Packages\5319275A.WhatsAppDesktop_cv1g1gvanyjgm\TempState\432FD02F951F393DF1591310E6811118\Imagen de WhatsApp 2024-06-07 a las 16.18.15_5b9c512d.jpg" alt="Sample Image" />
       </template>
       <div>
-        <div class="text-h6">Content title 3</div>
+        <div class="text-h6">Miguel Angel Montes de Oca</div>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
-      </div>
-    </v-timeline-item>
-
-    <v-timeline-item>
-      <template v-slot:opposite>
-        <img src="https://via.placeholder.com/200x400" alt="Sample Image" />
-      </template>
-      <div>
-        <div class="text-h6">Content title 4</div>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
+Nombre: Miguel Angel Montes de Oca     Descripción: Estudiante de la carrera de Negocios Digitales                                           Nacimiento: 12 de diciembre de 2002 Lugar: La Paz, Bolivia.              Colegio de egreso: Libertadores de America       </p>
       </div>
     </v-timeline-item>
 
@@ -127,7 +126,7 @@
 <style>
 img {
   width: 350px;
-  height: 250px;
+  height: 350px;
 }
 </style>
 
