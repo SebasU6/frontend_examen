668834b25981f1a0370a986e63d6a1534b8f5088
YaBoiKenobi12 <angelmiguel1297@gmail.com>
2024-06-07 17:17:44 -0400

    bordes de nosotros y mejor organizado xd

--- pages/nosotros/components/about.vue
+++ pages/nosotros/components/about.vue
@@ -2,58 +2,75 @@
   <v-timeline>
     <v-timeline-item>
       <template v-slot:opposite>
-        <img src="https://via.placeholder.com/200x400" alt="Sample Image" />
+        <img src="https://via.placeholder.com/200x400" alt="Sample Image" class="image" />
       </template>
       <div>
-        <div class="text-h6">Sebastian Uzqueda</div>
-        <p>
-          Nombre: Bruno Coss     Descripción: Estudiante de la carrera de Negocios y Ciencia de Datos                                            Nacimiento: 26 de agosto de 2005 Lugar: La Paz, Bolivia.              Colegio de egreso: San Ignacio 
+        <div class="name">Sebastian Uzqueda</div>
+        <p class="description">
+          Nombre: Bruno Coss<br>
+          Descripción: Estudiante de la carrera de Negocios Digitales UCB - ePC<br>
+          Nacimiento: 26 de agosto de 2005<br>
+          Lugar: La Paz, Bolivia.<br>
+          Colegio de egreso: San Ignacio 
         </p>
       </div>
     </v-timeline-item>
 
     <v-timeline-item>
       <template v-slot:opposite>
-        <img src="file:///C:/Users/MIGUEL%20ANGEL/Desktop/foto%20bruno%20para%20programacion.jpg" alt="Sample Image" />
+        <img src="file:///C:/Users/MIGUEL%20ANGEL/Desktop/foto%20bruno%20para%20programacion.jpg" alt="Bruno Image" class="image" />
       </template>
       <div>
-        <div class="text-h6">Bruno Coss</div>
-        <p>
-          Nombre: Bruno Coss     Descripción: Estudiante de la carrera de Negocios y Ciencia de Datos                                            Nacimiento: 26 de agosto de 2005 Lugar: La Paz, Bolivia.              Colegio de egreso: San Ignacio 
+        <div class="name">Bruno Coss</div>
+        <p class="description">
+          Nombre: Bruno Coss<br>
+          Descripción: Estudiante de la carrera de Negocios y Ciencia de Datos UCB - ePC<br>
+          Nacimiento: 26 de agosto de 2005<br>
+          Lugar: La Paz, Bolivia.<br>
+          Colegio de egreso: San Ignacio 
         </p>
       </div>
     </v-timeline-item>
 
     <v-timeline-item>
       <template v-slot:opposite>
-        <img src="https://via.placeholder.com/200x400" alt="Sample Image" />
+        <img src="c:\Users\MIGUEL ANGEL\AppData\Local\Packages\5319275A.WhatsAppDesktop_cv1g1gvanyjgm\TempState\B27B449FD22D309D80ACE80B9960F7D0\Imagen de WhatsApp 2024-06-07 a las 17.04.28_84ca14a0.jpg" alt="Gabriel Image" class="image" />
       </template>
       <div>
-        <div class="text-h6">Gabriel Villarroel</div>
-        <p>
-          Nombre: Bruno Coss     Descripción: Estudiante de la carrera de Negocios y Ciencia de Datos                                            Nacimiento: 26 de agosto de 2005 Lugar: La Paz, Bolivia.              Colegio de egreso: San Ignacio 
+        <div class="name">Gabriel Villarroel</div>
+        <p class="description">
+          Nombre: Gabriel Villarroel<br>
+          Descripción: Estudiante de la carrera de Negocios Digitales UCB - ePC<br>
+          Nacimiento: 03 de enero de 2006<br>
+          Lugar: La Paz, Bolivia.<br>
+          Colegio de egreso: Saint Andrews School
         </p>
       </div>
     </v-timeline-item>
 
     <v-timeline-item>
       <template v-slot:opposite>
-        <img src="c:\Users\MIGUEL ANGEL\AppData\Local\Packages\5319275A.WhatsAppDesktop_cv1g1gvanyjgm\TempState\432FD02F951F393DF1591310E6811118\Imagen de WhatsApp 2024-06-07 a las 16.18.15_5b9c512d.jpg" alt="Sample Image" />
+        <img src="c:\Users\MIGUEL ANGEL\AppData\Local\Packages\5319275A.WhatsAppDesktop_cv1g1gvanyjgm\TempState\432FD02F951F393DF1591310E6811118\Imagen de WhatsApp 2024-06-07 a las 16.18.15_5b9c512d.jpg" alt="Miguel Angel Image" class="image" />
       </template>
       <div>
-        <div class="text-h6">Miguel Angel Montes de Oca</div>
-        <p>
-Nombre: Miguel Angel Montes de Oca     Descripción: Estudiante de la carrera de Negocios Digitales                                           Nacimiento: 12 de diciembre de 2002 Lugar: La Paz, Bolivia.              Colegio de egreso: Libertadores de America       </p>
+        <div class="name">Miguel Angel Montes de Oca</div>
+        <p class="description">
+          Nombre: Miguel Angel Montes de Oca<br>
+          Descripción: Estudiante de la carrera de Negocios Digitales UCB - ePC<br>
+          Nacimiento: 12 de diciembre de 2002<br>
+          Lugar: La Paz, Bolivia.<br>
+          Colegio de egreso: Libertadores de America
+        </p>
       </div>
     </v-timeline-item>
 
     <v-timeline-item>
       <template v-slot:opposite>
-        <img src="https://img.freepik.com/foto-gratis/mujer-joven-alegre-que-tiene-descanso-mientras-trabaja-casa-sentado-escritorio-frente-computadora-portatil-abierta_343059-852.jpg" alt="Sample Image" />
+        <img src="https://img.freepik.com/foto-gratis/mujer-joven-alegre-que-tiene-descanso-mientras-trabaja-casa-sentado-escritorio-frente-computadora-portatil-abierta_343059-852.jpg" alt="Sample Image" class="image" />
       </template>
       <div>
-        <div class="text-h6">Mision</div>
-        <p>
+        <div class="name">Misión</div>
+        <p class="description">
           "Nuestra misión es facilitar y acortar el tiempo de búsqueda de viviendas proporcionando una plataforma web y una aplicación intuitivas y eficaces. Nos comprometemos a ofrecer una experiencia personalizada y segura, ayudando a nuestros usuarios a encontrar la casa o departamento ideal con facilidad y confianza. A través de innovación constante y un enfoque en las necesidades de nuestros clientes, nos esforzamos por ser la solución más confiable para encontrar el hogar perfecto."
         </p>
       </div>
@@ -61,23 +78,23 @@ Nombre: Miguel Angel Montes de Oca     Descripción: Estudiante de la carrera de
 
     <v-timeline-item>
       <template v-slot:opposite>
-        <img src="https://media.istockphoto.com/id/504481783/es/foto/dar-las-llaves-de-casa.jpg?s=612x612&w=0&k=20&c=j21NDOtk5hwkWCCm_iVTDMEFfZuyfxySJeali4Fsmm0=" alt="Sample Image" />
+        <img src="https://media.istockphoto.com/id/504481783/es/foto/dar-las-llaves-de-casa.jpg?s=612x612&w=0&k=20&c=j21NDOtk5hwkWCCm_iVTDMEFfZuyfxySJeali4Fsmm0=" alt="Sample Image" class="image" />
       </template>
       <div>
-        <div class="text-h6">Vision</div>
-        <p>
-          "Ser la plataforma líder a nivel mundial en la búsqueda de viviendas, transformando el proceso de encontrar la casa de tus sueños en una experiencia simple, rápida y agradable, donde cada persona tenga las llaves para abrir la puerta de sus sueños."  
+        <div class="name">Visión</div>
+        <p class="description">
+          "Ser la plataforma líder a nivel mundial en la búsqueda de viviendas, transformando el proceso de encontrar la casa de tus sueños en una experiencia simple, rápida y agradable, donde cada persona tenga las llaves para abrir la puerta de sus sueños."
         </p>
       </div>
     </v-timeline-item>
 
     <v-timeline-item>
       <template v-slot:opposite>
-        <img src="https://via.placeholder.com/200x400" alt="Sample Image" />
+        <img src="https://via.placeholder.com/200x400" alt="Sample Image" class="image" />
       </template>
       <div>
-        <div class="text-h6">Content title 7</div>
-        <p>
+        <div class="name">Content title 7</div>
+        <p class="description">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </p>
       </div>
@@ -85,11 +102,11 @@ Nombre: Miguel Angel Montes de Oca     Descripción: Estudiante de la carrera de
 
     <v-timeline-item>
       <template v-slot:opposite>
-        <img src="https://via.placeholder.com/200x400" alt="Sample Image" />
+        <img src="https://via.placeholder.com/200x400" alt="Sample Image" class="image" />
       </template>
       <div>
-        <div class="text-h6">Content title 8</div>
-        <p>
+        <div class="name">Content title 8</div>
+        <p class="description">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </p>
       </div>
@@ -97,11 +114,11 @@ Nombre: Miguel Angel Montes de Oca     Descripción: Estudiante de la carrera de
 
     <v-timeline-item>
       <template v-slot:opposite>
-        <img src="https://via.placeholder.com/200x400" alt="Sample Image" />
+        <img src="https://via.placeholder.com/200x400" alt="Sample Image" class="image" />
       </template>
       <div>
-        <div class="text-h6">Content title 9</div>
-        <p>
+        <div class="name">Content title 9</div>
+        <p class="description">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </p>
       </div>
@@ -109,11 +126,11 @@ Nombre: Miguel Angel Montes de Oca     Descripción: Estudiante de la carrera de
 
     <v-timeline-item>
       <template v-slot:opposite>
-        <img src="https://via.placeholder.com/200x400" alt="Sample Image" />
+        <img src="https://via.placeholder.com/200x400" alt="Sample Image" class="image" />
       </template>
       <div>
-        <div class="text-h6">Content title 10</div>
-        <p>
+        <div class="name">Content title 10</div>
+        <p class="description">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </p>
       </div>
@@ -124,9 +141,25 @@ Nombre: Miguel Angel Montes de Oca     Descripción: Estudiante de la carrera de
 <script setup></script>
 
 <style>
-img {
+.image {
   width: 350px;
   height: 350px;
+  border: 5px solid #333; /* Border color and thickness */
+  border-radius: 10px; /* Rounded corners */
+}
+
+.name {
+  font-family: 'Roboto', sans-serif;
+  font-size: 1.5rem;
+  color: #333; /* Dark color for names */
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+}
+
+.description {
+  font-family: 'Open Sans', sans-serif;
+  font-size: 1rem;
+  color: #666; /* Lighter color for descriptions */
+  line-height: 1.5;
 }
 </style>
-
