b0af208d0ceb9daef6c33e008412e24a80efd688
YaBoiKenobi12 <angelmiguel1297@gmail.com>
2024-06-07 16:51:45 -0400

    mision y vision

--- pages/nosotros/components/about.vue
+++ pages/nosotros/components/about.vue
@@ -7,7 +7,7 @@
       <div>
         <div class="text-h6">Sebastian Uzqueda</div>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Nombre: Bruno Coss     Descripción: Estudiante de la carrera de Negocios y Ciencia de Datos                                            Nacimiento: 26 de agosto de 2005 Lugar: La Paz, Bolivia.              Colegio de egreso: San Ignacio 
         </p>
       </div>
     </v-timeline-item>
@@ -31,7 +31,7 @@
       <div>
         <div class="text-h6">Gabriel Villarroel</div>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Nombre: Bruno Coss     Descripción: Estudiante de la carrera de Negocios y Ciencia de Datos                                            Nacimiento: 26 de agosto de 2005 Lugar: La Paz, Bolivia.              Colegio de egreso: San Ignacio 
         </p>
       </div>
     </v-timeline-item>
@@ -49,24 +49,24 @@ Nombre: Miguel Angel Montes de Oca     Descripción: Estudiante de la carrera de
 
     <v-timeline-item>
       <template v-slot:opposite>
-        <img src="https://via.placeholder.com/200x400" alt="Sample Image" />
+        <img src="https://img.freepik.com/foto-gratis/mujer-joven-alegre-que-tiene-descanso-mientras-trabaja-casa-sentado-escritorio-frente-computadora-portatil-abierta_343059-852.jpg" alt="Sample Image" />
       </template>
       <div>
-        <div class="text-h6">Content title 5</div>
+        <div class="text-h6">Mision</div>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          "Nuestra misión es facilitar y acortar el tiempo de búsqueda de viviendas proporcionando una plataforma web y una aplicación intuitivas y eficaces. Nos comprometemos a ofrecer una experiencia personalizada y segura, ayudando a nuestros usuarios a encontrar la casa o departamento ideal con facilidad y confianza. A través de innovación constante y un enfoque en las necesidades de nuestros clientes, nos esforzamos por ser la solución más confiable para encontrar el hogar perfecto."
         </p>
       </div>
     </v-timeline-item>
 
     <v-timeline-item>
       <template v-slot:opposite>
-        <img src="https://via.placeholder.com/200x400" alt="Sample Image" />
+        <img src="https://media.istockphoto.com/id/504481783/es/foto/dar-las-llaves-de-casa.jpg?s=612x612&w=0&k=20&c=j21NDOtk5hwkWCCm_iVTDMEFfZuyfxySJeali4Fsmm0=" alt="Sample Image" />
       </template>
       <div>
-        <div class="text-h6">Content title 6</div>
+        <div class="text-h6">Vision</div>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          "Ser la plataforma líder a nivel mundial en la búsqueda de viviendas, transformando el proceso de encontrar la casa de tus sueños en una experiencia simple, rápida y agradable, donde cada persona tenga las llaves para abrir la puerta de sus sueños."  
         </p>
       </div>
     </v-timeline-item>
